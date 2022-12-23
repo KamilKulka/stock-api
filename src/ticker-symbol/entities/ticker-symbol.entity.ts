e0bcf0ef13d4d82ea328d2916959a9fda3d5fda8
Kamil Kulka <kulka2190@gmail.com>
2022-12-23 21:35:21 +0100

    TickerSymbol entity modified. Modified entity and DB schema to match task requirements.

--- src/ticker-symbol/entities/ticker-symbol.entity.ts
+++ src/ticker-symbol/entities/ticker-symbol.entity.ts
@@ -1,23 +1,15 @@
 import { ObjectType, Field } from '@nestjs/graphql';
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, OneToMany, PrimaryColumn } from 'typeorm';
 import { Record } from '../../record/entities/record.entity';
 
 @Entity()
 @ObjectType()
 export class TickerSymbol {
-  @PrimaryGeneratedColumn('uuid')
-  @Field()
-  id: string;
-
-  @Column()
+  @PrimaryColumn()
   @Field()
   symbol: string;
 
-  @Column()
-  @Field()
-  currency: string;
-
   @OneToMany(() => Record, (record) => record.ticker)
-  @Field((type) => [Record], { nullable: true })
+  @Field(() => [Record], { nullable: true })
   records?: Record[];
 }
